df7e969636f28205c80a4433e0712d568b8cf62c
Eyebrock <eyebrock2913@gmail.com>
2023-03-10 12:13:51 -0600

    mdbadmin

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,7 @@ Route::get('/', function () {
 Route::get('/welcome', function () {
     return view('WelcomeHome');
 });
+
+Route::get('/adminview', function () {
+    return view('mainadmin');
+});
