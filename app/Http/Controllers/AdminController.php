7f5dd868723841b0a965a0f7521303d0ddb29685
Jose Montero <josmael0202@hotmail.com>
2023-03-20 15:00:41 -0600

    actualización + config para pruebas

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Http\Controllers;
+
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Http;
 
@@ -11,25 +12,24 @@ class AdminController extends Controller
 
     public function __construct()
     {
-        $this->baseUri = 'https://boom-phrygian-sceptre.glitch.me/api/v1/qr/';
-        $this->SbaseUri = 'https://boom-phrygian-sceptre.glitch.me/api/v1/usuario/report/';
+        //$aux = 'https://boom-phrygian-sceptre.glitch.me';
+        $aux = 'http://localhost:8001';
+        $this->baseUri = $aux . '/api/v1/qr/';
+        $this->SbaseUri = $aux . '/api/v1/usuario/report/';
     }
 
     public function getqr($id)
-{
-    $response = Http::post($this->baseUri , [
-        'link' => $this->SbaseUri . $id,
-    ]);
+    {
+        $response = Http::post($this->baseUri, [
+            'link' => $this->SbaseUri . $id,
+        ]);
 
-    if ($response->successful()) {
-        $qr = $response->body();
-        return view('qrcode', compact('qr'));
-    } else {
-        $errorMessage = $response->body();
-        return view('errorPage', compact('errorMessage'));
+        if ($response->successful()) {
+            $qr = $response->body();
+            return view('qrcode', compact('qr'));
+        } else {
+            $errorMessage = $response->body();
+            return view('errorPage', compact('errorMessage'));
+        }
     }
 }
-
-
-}
-
